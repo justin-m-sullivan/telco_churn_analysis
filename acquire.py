46f5cb886b8d7e518236e891610ddaa79fa6c493
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-03-05 17:04:13 -0600

    updating some typos and adding imports

---
+++ acquire.py
@@ -0,0 +1,36 @@
+import numpy as np
+import pandas as pd
+import os
+from env import host, user, password
+
+def get_connection(db, user=user, host=host, password=password):
+    '''
+    This function uses my info from my env file to
+    create a connection url to access the CodeUp db.
+    '''
+    return f'mysql+pymysql://{user}:{password}@{host}/{db}'
+
+
+def get_telco_data():
+    '''
+    This function reads in telco data from Codeup database and writes data to
+    a csv file, returns df.
+    '''
+    filename = 'telco.csv'
+    if os.path.isfile(filename):
+        return pd.read_csv(filename)
+
+    else:
+        #Read fresh data from db into a DataFrame.
+        df = pd.read_sql('''
+            SELECT * FROM customers
+            JOIN internet_service_types ON(customers.internet_service_type_id)
+            JOIN contract_types ON(customers.contract_type_id)
+            JOIN payment_types ON(customers.payment_type_id);
+            ''', 
+            get_connection('telco_churn'))
+        
+        #Write DataFrame to a csv file.
+        df.to_csv(filename)
+          
+    return df

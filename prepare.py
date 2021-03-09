85418329a4f923e1fd027a7d0b6a265118bc5758
Justin Sullivan <sullivan.justin.michael@gmail.com>
2021-03-09 11:38:00 -0600

    updating doc strings

--- prepare.py
+++ prepare.py
@@ -6,11 +6,22 @@ from env import host, user, password
 def prep_data(df):
     '''
     This function takes in the telco data frame and prepares it for analysis by:
-
-
+    * Setting index to customer_id
+    * Deals with the strings indicating "No internet / phone service" by replacing those values with 0
+    * Converts yes and no values to boolean int64 datatypes (0s and 1s) 
+    * Bins the 4 payment methods into two groups: Auto Pay Methods and Manual Pay Methods
+    * Encodes the gender feature and returns a gender_male column with booleans
+    * Drops dupicate columns for in favor of keeping the corresponding columns where the features are already encoded
+    * Deals with empty strings in the total_charges column. These values are empty because the corresponding 
+    customer's tenure is one month or less and total_charges have not accumulated yet. These values are filled with their corresponding monthly_charges.
+    total_charges column is then coverted to float64 datatype. 
     '''
+    
+    #Set Customer ID as the index
+    df.set_index('customer_id', drop=True, inplace=True)
+    
     #Convert strings in features that do not match "yes" or "no"
-    df.replace({'No internet service': 'No', 'No phone service':'No'}, inplace=True)
+    df.replace({'No internet service': "No" , 'No phone service': "No"}, inplace=True)
 
     #Convert features with yes or no to 0s and 1s
     df.replace({'Yes': 1, 'No': 0}, inplace=True)
@@ -32,8 +43,7 @@ def prep_data(df):
     df = pd.concat([df, dummies], axis=1)
 
     #Columns to drop
-    cols_to_drop = ['internet_service_type_id', 'contract_type_id', 
-    'gender','internet_service_type', 'contract_type', 'payment_type', 'customer_id']
+    cols_to_drop = ['gender','internet_service_type', 'contract_type', 'payment_type']
 
     #drop columns
     df.drop(columns=cols_to_drop, inplace=True)
